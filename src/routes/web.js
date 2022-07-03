136c1f4a3db95bf3737ea0bc70cd1ee38b27fe8c
Your Name <you@example.com>
2022-07-04 00:40:41 +0700

    save code

---
+++ src/routes/web.js
@@ -0,0 +1,14 @@
+import express from "express";
+import webControllers from '../controllers/webControllers'
+const router = express.Router()
+
+const initWebRoutes = (app)=> {
+    router.get('/', webControllers.getCRUD)
+    router.post('/postCRUD', webControllers.postCRUD)
+    router.get('/getInfoUserCRUD', webControllers.getInfoUserCRUD)
+    router.post('/putCRUD', webControllers.putCRUD)
+    router.get('/deleteCRUD', webControllers.deleteCRUD)
+    return app.use ('/', router)
+}
+
+export default initWebRoutes;
